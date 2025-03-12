54abda78e88fe7fd0b904caa7c01c4d546f927f9
tylerberlin <berlity0611@gmail.com>
2025-03-12 10:01:18 -0500

    Created paddle object

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -10,6 +10,7 @@ import GameplayKit
 
 class GameScene: SKScene {
     var ball = SKShapeNode()
+    var paddle = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         createBackground()
@@ -58,8 +59,19 @@ class GameScene: SKScene {
         addChild(ball) // add  ball object to the view
     }
     
+    func makePaddle() {
+        paddle.removeFromParent() //remove paddle, if it exists
+        paddle = SKSpriteNode(color: .white, size: CGSize(width: frame.width/4, height: 20))
+        paddle.position = CGPoint(x: frame.midX, y: frame.midY + 125)
+        paddle.name = "paddle"
+        paddle.physicsBody = SKPhysicsBody(rectangleOf: paddle.size)
+        paddle.physicsBody?.isDynamic = false
+        addChild(paddle)
+    }
+    
     func resetGame() {
         // this stuff happens before each game starts
         makeBall()
+        makePaddle()
     }
 }
